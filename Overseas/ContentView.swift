e33c167824a9e4eb5ec4feca3f18ce6fe9745f25
Samuel <samuelbsantosn@gmail.com>
2021-07-06 09:09:18 -0300

    Adding project

---
+++ Overseas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Overseas
+//
+//  Created by Samuel Brasileiro on 06/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
