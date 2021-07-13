7093c265a951a5019dbbce723dfa8bf731513d2d
Samuel <samuelbsantosn@gmail.com>
2021-07-13 09:43:31 -0300

    Creating category view

--- Overseas/View/Category/CategoryLearningView.swift
+++ Overseas/View/Category/CategoryLearningView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CategoryLearningView: View {
     @ObservedObject var learning: Learning
     var color: Color //lembrar de fazer referencia à color do category referente (não fizemos porque é string)
+    
     var body: some View {
         ZStack{
             VStack(alignment: .leading){
@@ -22,37 +23,53 @@ struct CategoryLearningView: View {
                         .padding(10)
                 }
                 .frame(height: 40)
-                Text(learning.emoji ?? "?")
-                    .font(.system(size: 60))
-                    .padding(11)
-                    .overlay(Circle().stroke(color, lineWidth: 2))
-                    .padding(.leading)
-                
-                Text(learning.name ?? "Name")
-                    .font(.largeTitle.bold())
-                    .padding(.leading)
-                Text("Pança cheia de Beatrix Lee")
+                ZStack(alignment: .topTrailing){
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(learning.emoji ?? "?")
+                                .font(.system(size: 60))
+                                .padding(11)
+                                .overlay(Circle().stroke(color, lineWidth: 2))
+                                .padding(.leading)
+                            
+                            Text(learning.name ?? "Name")
+                                .font(.largeTitle.bold())
+                                .padding(.leading)
+                            Text("Pança cheia de Beatrix Lee")
+                                
+                                .padding(.leading)
+                            Spacer()
+                            
+                            
+                            
+                            Text(String(Int(learning.estimatedTime / 60)) + " minutos • \(learning.dateFormatter.string(from: learning.creationDate!))" )
+                                .padding(.leading)
+                                .padding(.bottom, 20)
+                        }
+                        .padding([.leading,.top])
+                        Spacer()
+                    }
                     
-                    .padding(.leading)
-                Spacer()
-                    
-                
-                
-                Text(String(Int(learning.estimatedTime / 60)) + " minutos" )
-                    .padding(.leading)
-                    .padding(.bottom, 20)
+                    Text(Image(systemName: "pin.fill"))
+                        .font(.system(size: 28))
+                        .foregroundColor(color)
+                        .rotationEffect(.init(degrees: 45))
+                        .padding()
+                        .opacity(learning.isFixed ? 1 : 0)
+                        
+                }
             }
             Rectangle().stroke(color, lineWidth: 4)
             
         }
-        .frame(maxWidth: 300, maxHeight: 300)
+        
     }
 }
 
 struct CategoryLearningView_Previews: PreviewProvider {
     static var previews: some View {
         CategoryLearningView(learning: Learning(name: "Sushii", descriptionText: "Pança cheiaaa de Beatrix Lee", emoji: "🍣", estimatedTime: 1800, text: "Sei lá", context: AppDelegate.viewContext), color: .blue)
-                                .previewLayout(.fixed(width: 300, height: 300))
+                                .previewLayout(.fixed(width: 600, height: 300))
                                 .environment(\.horizontalSizeClass, .compact)
                                 .environment(\.verticalSizeClass, .compact)
     }
--- Overseas/ViewModel/CategoryEnvironment.swift
+++ Overseas/ViewModel/CategoryEnvironment.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import CoreData
+class CategoryEnvironment: ObservableObject{
+    
+    @Published var category: Category
+    
+    @Published var fixedLearnings: [Learning] = []
+    
+    @Published var allLearnings: [Learning] = []
+    
+    let context = AppDelegate.viewContext
+
+    init(/*category: Category*/){
+        //self.category = category
+        
+        let category = Category(name: "Culinária", color: "Preto", context: context)
+        
+        for j in (0..<10){
+            let learning = Learning(name: "Sushi \(j)", descriptionText: "Aprendi sushi poxa", emoji: "🍣", estimatedTime: 1, text: "Uhhh aprendi a fazer sushi hoje", context: context)
+            
+            if j % 5 == 0{
+                learning.isFixed = true
+            }
+            
+            category.addToLearnings(learning)
+            learning.category = category
+        }
+        self.category = category
+        
+        
+        
+        //fixedLearnings = (category.learnings?.allObjects as! [Learning]).filter({learning in
+        //                                                                            learning.isFixed == true})
+        
+        
+        allLearnings = (category.learnings?.allObjects as! [Learning])
+        
+        allLearnings.sort(by: {(learning1, learning2) in
+            learning1.isFixed && !learning2.isFixed
+        })
+    }
+    
+}
