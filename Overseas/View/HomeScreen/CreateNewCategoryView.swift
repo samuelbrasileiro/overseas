89b2a6d07039643af8542d635d934276a61aab51
Samuel <samuelbsantosn@gmail.com>
2021-07-13 12:00:14 -0300

    Creation of view of category creation

--- Overseas/View/HomeScreen/CreateNewCategoryView.swift
+++ Overseas/View/HomeScreen/CreateNewCategoryView.swift
@@ -8,13 +8,107 @@
 import SwiftUI
 
 struct CreateNewCategoryView: View {
+    
+    let env: HomeScreenEnvironment
+    
+    @State var name: String = ""
+    
+    let allColors: [Color] = [.orange, Color(.systemIndigo), .pink, .green, Color(.systemTeal)]
+    
+    @State var colorIndex: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading){
+            HStack{
+                Button(action:{
+                    env.didSelectNewCategory = false
+                }){
+                    Text("Cancelar")
+                }
+                Spacer()
+                Text("Adicionar Categoria")
+                    .bold()
+                Spacer()
+                Button(action:{
+                    env.didSelectNewCategory = false
+                    env.createNewCategory(name: name, colorIndex: colorIndex)
+                }){
+                    Text("Salvar")
+                        .bold()
+                        
+                }.disabled(name == "" ? true : false)
+                
+                
+            }
+            .padding()
+            .background(Color(.white))
+            
+            Text("Categoria")
+                .font(.title2.bold())
+                .padding()
+            
+            TextField("Nome da categoria", text: $name)
+            .padding()
+            .background(Color(.white))
+            HStack{
+                Spacer()
+                Text("A categoria ajuda a agrupar os aprendizados e ver o progresso em diferentes aspectos da vida")
+                .font(.footnote)
+                    .foregroundColor(.gray)
+                Spacer()
+            }
+            
+            Text("Cor")
+                .font(.title2.bold())
+                .padding()
+            
+            HStack{
+                
+                ForEach((0..<allColors.count)){index in
+                    Button(action:{
+                        self.colorIndex = index
+                    }){
+                        ZStack{
+                            Circle()
+                                .fill(allColors[index])
+                                .frame(width: 50, height: 50)
+                                .padding(4)
+                                .overlay(Circle().stroke().foregroundColor(allColors[index]).opacity(index == colorIndex ? 1 : 0)
+                                )
+                            Text(Image(systemName: "checkmark"))
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .opacity(index == colorIndex ? 1 : 0)
+                        }
+                    }
+                }
+                Spacer()
+                
+            }
+            .padding(.leading)
+            .padding(.vertical)
+            .background(Color(.white))
+            HStack{
+                Spacer()
+                Text("Os aprendizados referentes a essa categoria ficarão sinalizadas com a cor escolhida")
+                .font(.footnote)
+                    .foregroundColor(.gray)
+                Spacer()
+            }
+            
+            Spacer()
+        }
+        .background(Color(.systemGray6))
     }
 }
 
 struct CreateNewCategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateNewCategoryView()
+        CreateNewCategoryView(env: HomeScreenEnvironment())
+            //.previewDevice("iPad (8th generation)")
+            .previewLayout(.fixed(width: 600, height: 500))
+            .environment(\.horizontalSizeClass, .compact)
+            .environment(\.verticalSizeClass, .compact)
     }
 }
--- Overseas/ViewModel/HomeScreenEnvironment.swift
+++ Overseas/ViewModel/HomeScreenEnvironment.swift
@@ -7,30 +7,36 @@
 
 import Foundation
 import CoreData
+import SwiftUI
+
 class HomeScreenEnvironment: ObservableObject{
     @Published var categories: [Category] = []
     let context = AppDelegate.viewContext
     
     @Published var fixedLearnings: [Learning] = []
     @Published var allLearnings: [Learning] = []
+    
+    @Published var didSelectNewCategory = false
+    
+    
     init(){
         
-        for i in (0..<10){
-            let category = Category(name: "Culinária \(i)", color: "Preto", context: context)
-            
-            for j in (0..<10){
-                let learning = Learning(name: "Fazer sushi \(j)", descriptionText: "Aprendi sushi", emoji: "🍣", estimatedTime: 1, text: "Uhhh aprendi a fazer sushi hoje", context: context)
-                
-                if j % 3 == 0{
-                    learning.isFixed = true
-                }
-                
-                category.addToLearnings(learning)
-                learning.category = category
-            }
-            categories.append(category)
-            
-        }
+//        for i in (0..<10){
+//            let category = Category(name: "Culinária \(i)", color: 0, context: context)
+//
+//            for j in (0..<10){
+//                let learning = Learning(name: "Fazer sushi \(j)", descriptionText: "Aprendi sushi", emoji: "🍣", estimatedTime: 1, text: "Uhhh aprendi a fazer sushi hoje", context: context)
+//
+//                if j % 3 == 0{
+//                    learning.isFixed = true
+//                }
+//
+//                category.addToLearnings(learning)
+//                learning.category = category
+//            }
+//            categories.append(category)
+//
+//        }
         
         
         let fixed = categories.map({ category in
@@ -55,5 +61,28 @@ class HomeScreenEnvironment: ObservableObject{
         }
         
     }
+    
+    func createNewCategory(name: String, colorIndex: Int){
+        
+        if name == ""{
+            return
+        }
+        
+        let context = AppDelegate.viewContext
+        
+        
+        let category = Category(name: name, color: colorIndex, context: context)
+        
+        categories.append(category)
+        
+        do{
+            try context.save()
+        }catch{
+            print(error)
+        }
+        
+    }
+    
+    
 
 }
