a06d1d1d9874b96b7b8515becf66e301db983467
Beatrix <blsr3@academy.cin.ufpe.br>
2021-07-14 08:21:48 -0300

    Created emoji picker sctructure and started learning view

---
+++ Overseas/EmojiStructure.swift
@@ -0,0 +1,72 @@
+//
+//  EmojiStructure.swift
+//  Overseas
+//
+//  Created by Beatrix lee on 13/07/21.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+
+class UIEmojiTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setEmoji() {
+        _ = self.textInputMode
+    }
+    
+    override var textInputContextIdentifier: String? {
+           return ""
+    }
+    
+    override var textInputMode: UITextInputMode? {
+        for mode in UITextInputMode.activeInputModes {
+            if mode.primaryLanguage == "emoji" {
+                self.keyboardType = .default // do not remove this
+                return mode
+            }
+        }
+        return nil
+    }
+}
+
+struct EmojiTextField: UIViewRepresentable {
+    @Binding var text: String
+    var placeholder: String = ""
+    
+    func makeUIView(context: Context) -> UIEmojiTextField {
+        let emojiTextField = UIEmojiTextField()
+        emojiTextField.placeholder = placeholder
+        emojiTextField.text = text
+        emojiTextField.delegate = context.coordinator
+        return emojiTextField
+    }
+    
+    func updateUIView(_ uiView: UIEmojiTextField, context: Context) {
+        text = String(text.suffix(1))
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: EmojiTextField
+        
+        init(parent: EmojiTextField) {
+            self.parent = parent
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async { [weak self] in
+                self?.parent.text = textField.text ?? ""
+            }
+        }
+    }
+}
--- Overseas/View/LearningView.swift
+++ Overseas/View/LearningView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct LearningView: View {
+    
+    @State var title: String = ""
+    @State var description: String = ""
+    @State var emoji: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            HStack {
+                
+                EmojiTextField(text: $emoji, placeholder: "🍣")
+                    
+                
+                VStack{
+                    
+                    TextField("Insira seu título aqui", text: $title)
+                        .font(.largeTitle.bold())
+                    TextField("Insira sua descrição aqui", text: $description)
+                        .font(.footnote)
+                }
+            }
+        }
     }
 }
 
