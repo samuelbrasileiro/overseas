99b5e3810cfed63a49daded6f09ef06c0088d213
Beatrix <blsr3@academy.cin.ufpe.br>
2021-07-20 11:59:38 -0300

    Adding onboardingview with pagecontrol

--- Overseas/View/Onboarding/OnboardingView.swift
+++ Overseas/View/Onboarding/OnboardingView.swift
@@ -7,15 +7,124 @@
 
 import SwiftUI
 
-struct OnboardingView: View {
+struct OnboardingCardView: View {
+    
     @State var isPresented = false
+    @Binding var position: CGPoint
+    @Binding var scale: CGSize
+    @State var emoji: String
+    @State var color: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            VStack(alignment: .leading){
+                ZStack(alignment: .trailing){
+                    Rectangle()
+                        .fill(color)
+                    
+                }
+                .frame(height: 40)
+                
+                Text(emoji)
+                    .font(.system(size: 60))
+                    .padding(11)
+                    .overlay(Circle().stroke(color, lineWidth: 2))
+                    .padding(.leading)
+                
+                
+                Spacer()
+                
+            }
+            Rectangle().stroke(color, lineWidth: 4)
+            
+        }
+        .background(Color.white)
+        .frame(maxWidth: 250, maxHeight: 250)
+        .offset(x: position.x, y: position.y)
+        .scaleEffect(scale)
+    }
+    
+}
+
+struct OnboardingView: View {
+    
+    @State var scale = CGSize(width: 1, height: 1)
+    @State var positions = [CGPoint(x: -80,y: 20), CGPoint(x: -100,y: 70), CGPoint(x: -300,y: 0), CGPoint(x: -200,y: 80)]
+    var finalPositions = [CGPoint(x: 440,y: 0), CGPoint(x: 480,y: -30), CGPoint(x: 460,y: 0), CGPoint(x: 520,y: -10)]
+    
+    var body: some View {
+        
+        VStack {
+            
+            Spacer()
+            
+            ZStack{
+                
+                Image("BackFolder")
+                    .offset(x: 200, y: 0)
+                
+                
+                OnboardingCardView(position: $positions[0], scale: $scale, emoji: "🥦", color: .green)
+                OnboardingCardView(position: $positions[1], scale: $scale, emoji: "🤡", color: .red)
+                OnboardingCardView(position: $positions[2], scale: $scale, emoji: "🤝", color: .blue)
+                OnboardingCardView(position: $positions[3], scale: $scale, emoji: "🧚🏼‍♂️", color: .purple)
+                
+                Image("FrontFolder")
+                    .offset(x: 200, y: 0)
+                
+            }
+            .padding(.top, 100)
+            
+            
+        
+            
+            Text("Tudo em seu devido lugar")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 150)
+            
+            Text("Organize seus aprendizados em um espaço construído por você.")
+            
+            Spacer()
+        }
+        
+        .onAppear {
+            withAnimation(.spring().speed(0.5).delay(2)) {
+                positions = finalPositions
+                scale = CGSize(width: 0.4, height: 0.4)
+            }
+        }
     }
 }
 
+struct BrainOnboardingView: View {
+    var body: some View {
+        Text("Oi")
+    }
+}
+
+struct PageViewController: View {
+    var body: some View {
+        
+        TabView {
+            OnboardingView()
+            BrainOnboardingView()
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+ 
+    }
+}
+
+
+
+
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        PageViewController()
+            .previewLayout(.fixed(width: 1080, height: 810))
+            .environment(\.horizontalSizeClass, .compact)
+            .environment(\.verticalSizeClass, .compact)
     }
 }
