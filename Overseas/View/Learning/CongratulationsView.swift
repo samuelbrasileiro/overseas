eebbe9949723e60c07fa7cf35fe60d7a640f75dd
Elaine <ecf2@cin.ufpe.br>
2021-08-02 12:12:57 -0300

    Structure of view with animation done

---
+++ Overseas/View/Learning/CongratulationsView.swift
@@ -0,0 +1,82 @@
+//
+//  CongratulationsView.swift
+//  Overseas
+//
+//  Created by Elaine  Cruz on 02/08/21.
+//
+
+import SwiftUI
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 5
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
+    }
+}
+
+struct CongratulationsView: View {
+    @State var isPresented = false
+    @State var offset: CGSize = CGSize(width: -250, height: 100)
+    @State var ammount = 0
+    
+    var body: some View {
+        VStack{
+            ZStack{
+                Image("Target")
+                   
+                    .modifier(Shake(animatableData: CGFloat(ammount)))
+
+                    .onAppear(){
+                        withAnimation(.default.delay(0.35)){
+                            ammount += 1
+                        }
+                    }
+                
+                Image("Arrow")
+                    .offset(offset)
+                    .onAppear(){
+                        offset = CGSize(width: -130, height: 50)
+                    }
+                    .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.bottom, 50)
+
+            
+            Text("Parabéns!")
+                .font(.title)
+                .padding(.bottom,5)
+
+            
+            Text("Você conseguiu alcançar mais uma etapa de aprendizado")
+                .font(.subheadline)
+                .padding(.bottom, 20)
+            
+            Button {
+                self.isPresented = true
+            } label: {
+                ZStack{
+
+                    Rectangle()
+                        .foregroundColor(Color(.systemBlue))
+                        .frame(width: UIScreen.main.bounds.width/2, height: UIScreen.main.bounds.height/15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text("Concluir aprendizado")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                }
+               
+            }
+
+        }
+    }
+}
+
+struct CongratulationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CongratulationsView()
+    }
+}
